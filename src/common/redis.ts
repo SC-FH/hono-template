81b594896e23c7482304ff0e0781fb167cd4f6fc
FH <1425768167@qq.com>
2025-12-22 15:17:49 +0800

    添加redis重连机制

--- src/common/redis.ts
+++ src/common/redis.ts
@@ -1,8 +1,25 @@
 import { createClient } from 'redis'
 import { appConfig } from '../config/index.js'
+import { logger } from './logger.js';
 
 export const redis = await createClient({
     url: `redis://${appConfig.redis.host}:${appConfig.redis.port}`,
     username: appConfig.redis.username,
-    password: appConfig.redis.password
-}).connect()
+    password: appConfig.redis.password,
+    socket: {
+        reconnectStrategy(retries, cause) {
+            if (retries > 0) {
+                console.log(`redis 正在进行第 ${retries} 次重连`);
+            }
+            return 3000;
+        },
+    }
+})
+    .on('error', (err) => {
+        // logger.error(`Redis 连接错误: ${err}`);
+        console.error(`Redis 连接错误: ${err}`);
+    })
+    .on('connect', () => {
+        logger.info('Redis 连接成功');
+    })
+    .connect()
