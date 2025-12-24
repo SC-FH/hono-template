2054bbf6edff67614c91a9a8bb1323d116ae572d
FH <1425768167@qq.com>
2025-12-24 15:25:44 +0800

    添加注释

--- global.d.ts
+++ global.d.ts
@@ -10,8 +10,8 @@ declare global {
 declare module 'hono' {
     interface ContextVariableMap {
         // 定义Hono Context中的自定义存储的变量类型
-        userId: number;
-        isWS: boolean;
+        userId: number; //当前用户id
+        isWS: boolean;  //是否为websocket请求
     }
 }
 
