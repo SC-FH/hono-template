9216454e8c79df4ee93a0832479a463d5dac0198
FH <1425768167@qq.com>
2025-12-26 15:08:23 +0800

    添加角色校验配置

--- src/middleware/role.ts
+++ src/middleware/role.ts
@@ -1,19 +1,33 @@
 import { createMiddleware } from 'hono/factory'
 import { CustomException } from '../common/customException.js'
 
+type RoleMiddlewareOptions = {
+    /**
+     * 是否排除roles中的角色
+     */
+    isExclude: boolean
+}
+
 /**
  * 角色验证中间件
+ * @param roles 
+ * @param options 
+ * @returns 
  */
-export const roleMiddleware = (roles: string[]) => {
+export const roleMiddleware = (roles: string[], options: RoleMiddlewareOptions = { isExclude: false }) => {
     return createMiddleware((c, next) => {
 
         // const role = c.get('userRole')  //设置context用户角色（JWT层设置），从context中获取用户角色
-        const role = 'admin'
+        const role = 'admin';
 
-        if (roles.includes(role)) {
-            return next()
+        const hasAccess = options.isExclude
+            ? !roles.includes(role)
+            : roles.includes(role);
+
+        if (hasAccess) {
+            return next();
         }
 
-        throw new CustomException(403, '无权限访问')
+        throw new CustomException(403, '无权限访问');
     })
 }
